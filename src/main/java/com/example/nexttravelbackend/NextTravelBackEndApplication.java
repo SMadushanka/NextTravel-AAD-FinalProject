f77df789696dc46722c1a3b3e6c80cc0ada263a0
WSmadushanka <wijamu.maduu22@gmail.com>
2025-08-30 09:34:53 +0530

    add in github

---
+++ src/main/java/com/example/nexttravelbackend/NextTravelBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexttravelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTravelBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextTravelBackEndApplication.class, args);
+    }
+
+}
